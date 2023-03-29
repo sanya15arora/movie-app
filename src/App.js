ace290cb577243c00df92c092178b06c37564fcd
sanya15arora <128889967+sanya15arora@users.noreply.github.com>
2023-03-29 12:30:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
-import Movie from "./component/Movie";
+import React  from 'react'
+import Detail from './component/Detail'
+import Movie from './component/Movie'
 
-function App() {
-    return (
-        <Movie/>
-    );
+import { Routes, Route } from "react-router-dom";
+
+
+const App = () => {
+  return (
+  
+
+     <Routes>
+       <Route  path='/' element={<Movie/>}/>
+       <Route  path='/detail' element={<Detail/>}/>
+     </Routes>
+
+    
+    
+  )
 }
 
-export default App;
+
+export default App
+
+
